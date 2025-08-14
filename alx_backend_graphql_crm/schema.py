f2dc6797eaa9617c2a917743e621e2f710c67c2c
rihanas <rihanna.socials@gmail.com>
2025-08-14 20:01:45 +0300

    update

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,9 +1,13 @@
-
 import graphene 
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String()
-    def resolve_hello(root, info):
+    
+    def resolve_hello(self, info):
         return "Hello, GraphQL!"
 
-schema = graphene.Schema(query=Query)
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django_filters',
 ]
 
+GRAPHENE = {
+    'SCHEMA': 'alx_backend_graphql_crm.schema.schema',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
